263b53873a98ec05cf008ec640925f4f8c2c1d5e
Lennart Landsmeer <lennart_96@hotmail.com>
2014-04-06 20:29:28 +0200

    help, bugfix

--- con.sh
+++ con.sh
@@ -1,7 +1,34 @@
 set -e
 
+verbose=false
+
 cmd=$1
 shift
+
+if [ "$cmd" == '-v' ]; then
+    verbose=true
+    cmd=$1
+    shift
+elif [ "$cmd" == '-h' ]; then
+    echo "usage: $0 [-v] program arguments*
+       $0 -h
+
+example:
+       $ $0 echo a b c d e f
+       results on a 4 core system in:
+           echo a b &
+           echo c d &
+           echo e &
+           echo f &
+           wait
+       $ $0 'echo -n' a b c d
+           echo -n a &
+           echo -n b &
+           echo -n c &
+           echo -n d &
+           wait"
+    exit 0
+fi
 
 count=$#
 cores=$(nproc)
@@ -20,12 +47,12 @@ do
     {
         $cmd "${@:$start:$length}"
         result=$?
-        [ $result -eq 0 ] && echo " -> task $i done"
-      ! [ $result -eq 0 ] && echo " -> task $i errorcode $result"
+        [ $result -eq 0 ] && $verbose && echo " -> task $i done"
+      ! [ $result -eq 0 ] && $verbose &&echo " -> task $i errorcode $result"
     } &
 
     (( start += length))
-    (( rem   -= 1))
+    (( rem   -= 1)) || true
 done
 
 wait
