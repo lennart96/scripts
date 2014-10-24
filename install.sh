9779bd6ae83918aafc289178b7edf0f6c43de82f
Lennart Landsmeer <lennart_96@hotmail.com>
2014-10-24 16:52:32 +0200

    keep sudo rights in install

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
+sudo -v
+
+# keep sudo rights
+while true
+do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+done 2> /dev/null &
+
 for group in $(cat groups)
 do
     sudo gpasswd -a $USER $group
 done
-
+sudo pacman -S --needed --noconfirm zsh
+sudo chsh -s /usr/bin/zsh lennart
 sudo pacman -S --needed --noconfirm $(cat pac)
-pacaur -S -a --noconfirm --noedit $(cat aur)
+pacaur -S -a --noconfirm --noedit --insecure $(cat aur)
