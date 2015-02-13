6a898f33e1d5acba595d5a3b56695687d1792f89
Lennart Landsmeer <lennart_96@hotmail.com>
2015-02-13 23:58:16 +0000

    unspectre

--- baraction.sh
+++ baraction.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+setxkbmap -option 'caps:swapescape'
 while true
 do
     free -h | awk '$1 ~ /Mem/ { printf("  %s", $3); }'
@@ -6,7 +7,7 @@ do
     df -h | awk '$6=="/"||$6=="/home" {printf("  %s", $4);}'
     echo -n '  -  '
     echo -n $(ip addr show wlp2s0 scope global up | grep inet | cut -d' ' -f6)
-    echo -n '  '
+    echo -n ' '
     echo -n $(nmcli -t -f GENERAL.CONNECTION d sh wlp2s0 | cut -d':' -f2)
     echo -n '  -  '
     echo -n $(dropbox status)
@@ -14,6 +15,8 @@ do
     echo -n $(cat /sys/class/power_supply/BAT0/capacity)%
     echo -n '  -  '
     echo -n $(date +"%s  %Y-%m-%d %T %Z")
+    echo -n '  -  '
+    echo -n  $(amixer sget Master | awk -F"[][]" '/dB/ { print $2, $6}')
     echo -n '  -  '
     echo -n $(ps h -u "$USER" o 'comm' --sort '-pmem' | head -n 7)
     echo
