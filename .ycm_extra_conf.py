7730d18e18ffe8e0a891bc92ae01368409cfd3f2
Lennart Landsmeer <lennart@landsmeer.email>
2015-09-22 22:20:42 +0200

    better ycm conf

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,6 +1,19 @@
-flags = ( '-Wall -Wextra -Werror -std=c99 -x c -I .'.split()
-        + '-isystem /usr/include -isystem /usr/lib/clang/3.5.1/include'.split()
-        + ['-I/usr/include/libevdev-1.0/libevdev/', '-I/usr/include/libbsd'])
+flags = '''
+-Wall
+-Wextra
+-Werror
+-std=c99
+-x
+c
+-I
+.
+-isystem
+/usr/include
+-isystem
+/usr/lib/clang/3.5.1/include
+-I/usr/include/libevdev-1.0/libevdev/
+-I/usr/include/libbsd
+'''.splitlines()
 
 def FlagsForFile(filename, **kw):
     return {
